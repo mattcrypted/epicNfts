ae4db47540c069751d1381b79b55bfe855a8245d
mattcrypted <106237789+mattcrypted@users.noreply.github.com>
2022-10-19 13:28:48 +0100

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const main = async()=>{
+    const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNft');
+    const nftContract = await nftContractFactory.deploy();
+    await nftContract.deployed();
+    console.log("contract got deployed to",nftContract.address);
+
+    let txn = await nftContract.makeAnEpicNFT();
+    await txn.wait();
+    console.log("minted NFT #1")
+
+    
+}
+
+
+const runMain = async()=>{
+    try{
+        await main();
+        process.exit(0);
+    }catch(error){
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+runMain();
